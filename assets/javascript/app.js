35ee6506aba26264fe7c1908d03fde6f3c64b8ba
Meghan Mullally <ms.mullally27@gmail.com>
2019-06-27 19:07:37 -0700

    fixed the firebase link, added firebase js to the js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDJ7N7O3iETaUpds58R9U6aw9thZTz_LQ0",
+  authDomain: "traintime-2f3aa.firebaseapp.com",
+  databaseURL: "https://traintime-2f3aa.firebaseio.com",
+  projectId: "traintime-2f3aa",
+  storageBucket: "",
+  messagingSenderId: "250601739648",
+  appId: "1:250601739648:web:b8ec3a7a1cde8025"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
